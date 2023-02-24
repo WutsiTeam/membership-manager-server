7bde651bb8ed11337cd99e42447c76e496d929c3
wutsi <herve.tchepannou@wutsi.com>
2023-02-24 17:45:50 -0500

    handle username dupicate

--- src/main/kotlin/com/wutsi/membership/manager/workflow/UpdateMemberAttributeWorkflow.kt
+++ src/main/kotlin/com/wutsi/membership/manager/workflow/UpdateMemberAttributeWorkflow.kt
@@ -1,17 +1,25 @@
 package com.wutsi.membership.manager.workflow
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.wutsi.event.EventURN
 import com.wutsi.event.MemberEventPayload
 import com.wutsi.membership.access.dto.UpdateAccountAttributeRequest
+import com.wutsi.membership.access.error.ErrorURN
 import com.wutsi.membership.manager.dto.UpdateMemberAttributeRequest
+import com.wutsi.platform.core.error.Error
+import com.wutsi.platform.core.error.ErrorResponse
+import com.wutsi.platform.core.error.exception.ConflictException
 import com.wutsi.platform.core.stream.EventStream
 import com.wutsi.workflow.RuleSet
 import com.wutsi.workflow.WorkflowContext
+import feign.FeignException
 import org.springframework.stereotype.Service
 
 @Service
-class UpdateMemberAttributeWorkflow(eventStream: EventStream) :
-    AbstractMembershipWorkflow<UpdateMemberAttributeRequest, Unit>(eventStream) {
+class UpdateMemberAttributeWorkflow(
+    private val mapper: ObjectMapper,
+    eventStream: EventStream,
+) : AbstractMembershipWorkflow<UpdateMemberAttributeRequest, Unit>(eventStream) {
     override fun getEventType(request: UpdateMemberAttributeRequest, response: Unit, context: WorkflowContext) =
         EventURN.MEMBER_ATTRIBUTE_UPDATED.urn
 
@@ -23,12 +31,35 @@ class UpdateMemberAttributeWorkflow(eventStream: EventStream) :
     override fun getValidationRules(request: UpdateMemberAttributeRequest, context: WorkflowContext) = RuleSet.NONE
 
     override fun doExecute(request: UpdateMemberAttributeRequest, context: WorkflowContext) {
-        membershipAccessApi.updateAccountAttribute(
-            id = getCurrentAccountId(context),
-            request = UpdateAccountAttributeRequest(
-                name = request.name,
-                value = request.value,
-            ),
-        )
+        try {
+            membershipAccessApi.updateAccountAttribute(
+                id = getCurrentAccountId(context),
+                request = UpdateAccountAttributeRequest(
+                    name = request.name,
+                    value = request.value,
+                ),
+            )
+        } catch (ex: FeignException) {
+            val errorResponse = toErrorResponse(ex)
+            if (errorResponse?.error?.code == ErrorURN.NAME_ALREADY_ASSIGNED.urn) {
+                throw ConflictException(
+                    error = Error(
+                        code = com.wutsi.error.ErrorURN.USERNAME_ALREADY_ASSIGNED.urn,
+                        data = mapOf(
+                            "name" to (request.value ?: ""),
+                        ),
+                    ),
+                )
+            } else {
+                throw ex
+            }
+        }
     }
+
+    private fun toErrorResponse(ex: FeignException): ErrorResponse? =
+        try {
+            mapper.readValue(ex.contentUTF8(), ErrorResponse::class.java)
+        } catch (e: Exception) {
+            null
+        }
 }
--- src/test/kotlin/com/wutsi/membership/manager/endpoint/UpdateMemberAttributeControllerTest.kt
+++ src/test/kotlin/com/wutsi/membership/manager/endpoint/UpdateMemberAttributeControllerTest.kt
@@ -1,19 +1,25 @@
 package com.wutsi.membership.manager.endpoint
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.doThrow
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import com.wutsi.event.EventURN
 import com.wutsi.event.MemberEventPayload
 import com.wutsi.membership.access.dto.GetAccountResponse
 import com.wutsi.membership.access.dto.UpdateAccountAttributeRequest
+import com.wutsi.membership.access.error.ErrorURN
 import com.wutsi.membership.manager.Fixtures
 import com.wutsi.membership.manager.dto.UpdateMemberAttributeRequest
+import com.wutsi.platform.core.error.ErrorResponse
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.http.HttpStatus
+import org.springframework.web.client.HttpClientErrorException
 import kotlin.test.assertEquals
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -50,6 +56,24 @@ class UpdateMemberAttributeControllerTest : AbstractSecuredControllerTest() {
             EventURN.MEMBER_ATTRIBUTE_UPDATED.urn,
             MemberEventPayload(accountId = ACCOUNT_ID),
         )
+    }
+
+    @Test
+    fun testAlreadyAssigned() {
+        // GIVEN
+        val e = createFeignNotFoundException(ErrorURN.NAME_ALREADY_ASSIGNED.urn)
+        doThrow(e).whenever(membershipAccess).updateAccountAttribute(any(), any())
+
+        // WHEN
+        val ex = assertThrows<HttpClientErrorException> {
+            rest.postForEntity(url(), request, Any::class.java)
+        }
+
+        // THEN
+        assertEquals(HttpStatus.CONFLICT, ex.statusCode)
+
+        val response = ObjectMapper().readValue(ex.responseBodyAsString, ErrorResponse::class.java)
+        assertEquals(com.wutsi.error.ErrorURN.USERNAME_ALREADY_ASSIGNED.urn, response.error.code)
     }
 
     private fun url() = "http://localhost:$port/v1/members/attributes"
