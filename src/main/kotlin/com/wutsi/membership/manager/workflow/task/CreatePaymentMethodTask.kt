1d4898e1d35e495ab8b582dd313144cdf7b16937
wutsi <herve.tchepannou@wutsi.com>
2023-03-11 08:14:18 -0500

    ignore error when assigning payment method

--- src/main/kotlin/com/wutsi/membership/manager/workflow/task/CreatePaymentMethodTask.kt
+++ src/main/kotlin/com/wutsi/membership/manager/workflow/task/CreatePaymentMethodTask.kt
@@ -9,6 +9,7 @@ import com.wutsi.workflow.WorkflowContext
 import com.wutsi.workflow.engine.Workflow
 import com.wutsi.workflow.engine.WorkflowEngine
 import com.wutsi.workflow.util.WorkflowIdGenerator
+import feign.FeignException
 import org.springframework.stereotype.Service
 import javax.annotation.PostConstruct
 
@@ -38,16 +39,20 @@ class CreatePaymentMethodTask(
         ).paymentProviders
 
         if (providers.size == 1) {
-            checkoutAccessApi.createPaymentMethod(
-                request = CreatePaymentMethodRequest(
-                    accountId = account.id,
-                    type = providers[0].type,
-                    number = account.phone.number,
-                    country = account.country,
-                    ownerName = account.displayName,
-                    providerId = providers[0].id,
-                ),
-            )
+            try {
+                checkoutAccessApi.createPaymentMethod(
+                    request = CreatePaymentMethodRequest(
+                        accountId = account.id,
+                        type = providers[0].type,
+                        number = account.phone.number,
+                        country = account.country,
+                        ownerName = account.displayName,
+                        providerId = providers[0].id,
+                    ),
+                )
+            } catch (ex: FeignException) {
+                // Ignore the error
+            }
         }
     }
 }
